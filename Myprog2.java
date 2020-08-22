1c495d9e847f430b2691303ea7f1525083e28b20
mzamfirdaus <mohdzamfirdaus99@gmail.com>
2020-08-22 14:15:31 +0800

    Update Myprog2.java

--- Myprog2.java
+++ Myprog2.java
@@ -1,13 +1,8 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+
 
 public class Myprog2
 {
     public static void main(String[] args){ 
-        Scanner input = new Scanner(System.in);
-        Employee Employee1 = new Employee("Bob","Male",114);
-        Employee Employee2 = new Employee("Thomas","Male");
-        System.out.println(Employee1.toString());
-        System.out.println(Employee2.toString());
+        System.out.println("test");
     }
 }

01ee9e85796ac4b2080f138359f833be1df2efce
mzamfirdaus <mohdzamfirdaus99@gmail.com>
2020-09-03 22:41:08 +0800

    login and sign up class
    1. login
    2. sign up

--- Myprog2.java
+++
@@ -1,8 +0,0 @@
-
-
-public class Myprog2
-{
-    public static void main(String[] args){ 
-        System.out.println("test");
-    }
-}
---
+++ src/SignUp.java
@@ -0,0 +1,50 @@
+package src;
+
+import java.util.*;
+import java.io.File;  // Import the File class
+import java.io.FileWriter;
+import java.io.Writer;
+import java.io.BufferedWriter;
+
+
+public class RegisterUser{
+	
+	private static Scanner x;
+
+	public boolean registerNewUser(String username, String password, String password2, String filepath){
+		boolean success = false;
+		String tempUsername = "";
+		try{
+			Writer output;
+			x = new Scanner(new File(filepath));
+			x.useDelimiter("[,\n]");	
+			while(x.hasNext()){
+				tempUsername = x.next();
+				
+				if(tempUsername.trim().equals(username)){
+					return false;
+				}					
+			}
+			if(password != password2){
+				return false;
+			}
+			if(username == "" || password=="" || username == null || password==null){
+				return false;
+			}
+			else{
+				output = new BufferedWriter(new FileWriter(filepath, true));
+				output.append("\n"+username+","+password);
+				output.close();
+				/*
+				fileWrite.write(username+","+password);
+				fileWrite.close();
+				*/
+				success=true;
+			}
+		}
+		catch(Exception e){
+			
+		}
+		return success;
+	} 
+}

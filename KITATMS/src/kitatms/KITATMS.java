db426676d2d378782950697a1ee00760f668955d
adizafri2000 <61119610+adizafri2000@users.noreply.github.com>
2020-09-04 00:16:46 +0800

    Setup

--- KITATMS/src/kitatms/KITATMS.java
+++ KITATMS/src/kitatms/KITATMS.java
@@ -5,17 +5,32 @@
  */
 package kitatms;
 
+import java.sql.SQLException;
+
 /**
  *
  * @author adiza
  */
 public class KITATMS {
 
-    /**
+    /**^
      * @param args the command line arguments
+     * @throws java.sql.SQLException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // TODO code application logic here
+        System.out.println("Adi was here");
+        DBConnection tester = new DBConnection();
+        System.out.println(tester.readPassword());
+        /*System.out.println(tester.isConnectedMySQL());
+        System.out.println(tester.isConnectedDB());
+        if(!tester.isConnectedDB()){
+            tester.setupDB();
+            System.out.println(tester.isConnectedDB());
+            tester.setupTables();
+        }
+        System.out.println("Program ended. View KITATMS schema in MySQL for re-checking.");*/
+        //tester.clearData();
     }
-    
+
 }

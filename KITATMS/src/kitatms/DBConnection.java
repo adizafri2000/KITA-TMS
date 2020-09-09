ca6c9c69d95b4776e775c43f706f8d1b6bd7222f
adizafri2000 <61119610+adizafri2000@users.noreply.github.com>
2020-09-09 21:27:04 +0800

    Zd

--- KITATMS/src/kitatms/DBConnection.java
+++ KITATMS/src/kitatms/DBConnection.java
@@ -209,21 +209,17 @@ public class DBConnection {
 "    courseName varchar(50) not null,\n" +
 "    courseStart Date not null,\n" +
 "    courseEnd Date not null,\n" +
-"    accountID varchar(10) not null unique references account(accountID)\n" +
+"    accountID varchar(10) not null references account(accountID)\n" +
 ");";
                 
                 String enrollment = "CREATE TABLE `kitatms`.`enrollment` (\n" +
 "  `accountID` VARCHAR(10) NOT NULL,\n" +
 "  `courseID` VARCHAR(7) NOT NULL,\n" +
 "  PRIMARY KEY (`accountID`, `courseID`),\n" +
-"  UNIQUE INDEX `accountID_UNIQUE` (`accountID` ASC) VISIBLE,\n" +
-"  UNIQUE INDEX `courseID_UNIQUE` (`courseID` ASC) VISIBLE,\n" +
-"  CONSTRAINT `accountID`\n" +
 "    FOREIGN KEY (`accountID`)\n" +
 "    REFERENCES `kitatms`.`account` (`accountID`)\n" +
 "    ON DELETE NO ACTION\n" +
 "    ON UPDATE NO ACTION,\n" +
-"  CONSTRAINT `courseID`\n" +
 "    FOREIGN KEY (`courseID`)\n" +
 "    REFERENCES `kitatms`.`course` (`courseID`)\n" +
 "    ON DELETE NO ACTION\n" +
@@ -231,13 +227,13 @@ public class DBConnection {
                 
                 String report = "create table report(\n" +
 "	reportID varchar(13) primary key not null unique,\n" +
-"	courseID varchar(7) not null unique references course(courseID)\n" +
+"	courseID varchar(7) not null references course(courseID)\n" +
 ");";
                 
                 String learningmaterial = "create table learningmaterial(\n" +
 "	learningmaterialID varchar(10) primary key not null unique,\n" +
 "    learningmaterialName varchar(50) not null,\n" +
-"    courseID varchar(7) not null unique references course(courseID)\n" +
+"    courseID varchar(7) not null references course(courseID)\n" +
 ");";
                 
                 String assessment = "create table assessment(\n" +
@@ -252,7 +248,6 @@ public class DBConnection {
 "  `accountID` VARCHAR(10) NOT NULL,\n" +
 "  `viewstatus` VARCHAR(1) NULL,\n" +
 "  UNIQUE INDEX `learningmaterialID_UNIQUE` (`learningmaterialID` ASC) VISIBLE,\n" +
-"  UNIQUE INDEX `accountID_UNIQUE` (`accountID` ASC) VISIBLE,\n" +
 "    FOREIGN KEY (`learningmaterialID`)\n" +
 "    REFERENCES `kitatms`.`learningmaterial` (`learningmaterialID`)\n" +
 "    ON DELETE NO ACTION\n" +
@@ -317,9 +312,90 @@ public class DBConnection {
      */
     public void insertDummyData(){
         try (Statement stat = con.createStatement()) {
+            // error probably sebab string length is way too long
                 //SQL Insert query
-                String dbop = "INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('0123456789', '1', 'dummytrainer1');";
+                stat.execute("INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('0123456789', '1', 'dummytrainer1');\n");
+                String dbop = "INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('0123456789', '1', 'dummytrainer1');\n" +
+"INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('1234567890', '1', 'dummytrainer2');\n" +
+"INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('2345678901', '1', 'dummytrainer3');\n" +
+"INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('1181101286', '2', 'adi');\n" +
+"INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('1181101148', '2', 'zam');\n" +
+"INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('1181101256', '2', 'hadya');\n" +
+"INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('1181101272', '2', 'wahafizah');\n" +
+"INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('1181100008', '2', 'student08');\n" +
+"INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('1181100009', '2', 'student09');\n" +
+"INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('1181100010', '2', 'student10');\n" +
+"INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('1181100011', '2', 'student11');\n" +
+"INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('1181100012', '2', 'student12');\n" +
+"INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('1181100013', '2', 'student13');\n" +
+"INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('1181100014', '2', 'student14');\n" +
+"INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('1181100015', '2', 'student15');\n" +
+"INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('1181100016', '2', 'student16');\n" +
+"INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('1181100017', '2', 'student17');\n" +
+"INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('1181100018', '2', 'student18');\n" +
+"INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('1181100019', '2', 'student19');\n" +
+"INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('1181100020', '2', 'student20');\n" +
+"INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('1181100021', '2', 'student21');\n" +
+"INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('1181100022', '2', 'student22');\n" +
+"INSERT INTO `kitatms`.`account` (`accountID`, `accountType`, `accountPassword`) VALUES ('1181100023', '2', 'student23');";
                 stat.execute(dbop);
+                
+                dbop = "INSERT INTO `kitatms`.`trainer` (`trainerName`, `accountID`) VALUES ('Yagami Hikari', '0123456789');\n" +
+"INSERT INTO `kitatms`.`trainer` (`trainerName`, `accountID`) VALUES ('Nathan Freihofer', '1234567890');\n" +
+"INSERT INTO `kitatms`.`trainer` (`trainerName`, `accountID`) VALUES ('Izara Aishah', '2345678901');";
+                stat.execute(dbop);
+                
+                dbop = "INSERT INTO `kitatms`.`trainee` (`traineeName`, `accountID`) VALUES ('Adi Zafri Bin Mohd Tahir', '1181101286');\n" +
+"INSERT INTO `kitatms`.`trainee` (`traineeName`, `accountID`) VALUES ('Mohamad Zamfirdaus Bin Mohd Saberi', '1181101148');\n" +
+"INSERT INTO `kitatms`.`trainee` (`traineeName`, `accountID`) VALUES ('Hadya Ayeisha Binti Marzuki', '1181101256');\n" +
+"INSERT INTO `kitatms`.`trainee` (`traineeName`, `accountID`) VALUES ('Nur Wahafizah Binti Ahmadi', '1181101272');\n" +
+"INSERT INTO `kitatms`.`trainee` (`traineeName`, `accountID`) VALUES ('Brooke Monk', '1181100008');\n" +
+"INSERT INTO `kitatms`.`trainee` (`traineeName`, `accountID`) VALUES ('Bella Poarch', '1181100009');\n" +
+"INSERT INTO `kitatms`.`trainee` (`traineeName`, `accountID`) VALUES ('Jeff Mancuso', '1181100010');\n" +
+"INSERT INTO `kitatms`.`trainee` (`traineeName`, `accountID`) VALUES ('Orimoto Izumi', '1181100011');\n" +
+"INSERT INTO `kitatms`.`trainee` (`traineeName`, `accountID`) VALUES ('Jabami Yumeko', '1181100012');\n" +
+"INSERT INTO `kitatms`.`trainee` (`traineeName`, `accountID`) VALUES ('Yumemite Yumemi', '1181100013');\n" +
+"INSERT INTO `kitatms`.`trainee` (`traineeName`, `accountID`) VALUES ('Matsuda Takato', '1181100014');\n" +
+"INSERT INTO `kitatms`.`trainee` (`traineeName`, `accountID`) VALUES ('Jung Yerin', '1181100015');\n" +
+"INSERT INTO `kitatms`.`trainee` (`traineeName`, `accountID`) VALUES ('Heinz Doofenshmirtz', '1181100016');\n" +
+"INSERT INTO `kitatms`.`trainee` (`traineeName`, `accountID`) VALUES ('Eugene Krabs', '1181100017');\n" +
+"INSERT INTO `kitatms`.`trainee` (`traineeName`, `accountID`) VALUES ('Phil Swift', '1181100018');\n" +
+"INSERT INTO `kitatms`.`trainee` (`traineeName`, `accountID`) VALUES ('Seth Rollins', '1181100019');\n" +
+"INSERT INTO `kitatms`.`trainee` (`traineeName`, `accountID`) VALUES ('Brock Lesnar Jr', '1181100020');\n" +
+"INSERT INTO `kitatms`.`trainee` (`traineeName`, `accountID`) VALUES ('Ronald Weasley', '1181100021');\n" +
+"INSERT INTO `kitatms`.`trainee` (`traineeName`, `accountID`) VALUES ('Chris Sutherland', '1181100022');\n" +
+"INSERT INTO `kitatms`.`trainee` (`traineeName`, `accountID`) VALUES ('Zoe LaVerne', '1181100023');";
+                stat.execute(dbop);
+                
+                dbop = "INSERT INTO `kitatms`.`course` (`courseID`, `courseName`, `courseStart`, `courseEnd`, `accountID`) VALUES ('PSP5521', 'Introduction to Digital Systems', '2020-10-05', '2020-11-01', '2345678901');\n" +
+"INSERT INTO `kitatms`.`course` (`courseID`, `courseName`, `courseStart`, `courseEnd`, `accountID`) VALUES ('PPZ2290', 'Introduction to Physics', '2020-10-14', '2020-11-13', '1234567890');";
+                stat.execute(dbop);
+                
+                dbop = "INSERT INTO `kitatms`.`enrollment` (`accountID`, `courseID`) VALUES ('1181101286', 'PPZ2290');\n" +
+"INSERT INTO `kitatms`.`enrollment` (`accountID`, `courseID`) VALUES ('1181100012', 'PPZ2290');\n" +
+"INSERT INTO `kitatms`.`enrollment` (`accountID`, `courseID`) VALUES ('1181100017', 'PPZ2290');\n" +
+"INSERT INTO `kitatms`.`enrollment` (`accountID`, `courseID`) VALUES ('1181101148', 'PPZ2290');\n" +
+"INSERT INTO `kitatms`.`enrollment` (`accountID`, `courseID`) VALUES ('1181100019', 'PPZ2290');\n" +
+"INSERT INTO `kitatms`.`enrollment` (`accountID`, `courseID`) VALUES ('1181100011', 'PPZ2290');\n" +
+"INSERT INTO `kitatms`.`enrollment` (`accountID`, `courseID`) VALUES ('1181100014', 'PPZ2290');\n" +
+"INSERT INTO `kitatms`.`enrollment` (`accountID`, `courseID`) VALUES ('1181100015', 'PPZ2290');\n" +
+"INSERT INTO `kitatms`.`enrollment` (`accountID`, `courseID`) VALUES ('1181100018', 'PPZ2290');\n" +
+"INSERT INTO `kitatms`.`enrollment` (`accountID`, `courseID`) VALUES ('1181100016', 'PPZ2290');\n" +
+"INSERT INTO `kitatms`.`enrollment` (`accountID`, `courseID`) VALUES ('1181101272', 'PPZ2290');\n" +
+"INSERT INTO `kitatms`.`enrollment` (`accountID`, `courseID`) VALUES ('1181100008', 'PPZ2290');\n" +
+"INSERT INTO `kitatms`.`enrollment` (`accountID`, `courseID`) VALUES ('1181100022', 'PPZ2290');\n" +
+"INSERT INTO `kitatms`.`enrollment` (`accountID`, `courseID`) VALUES ('1181100009', 'PSP5521');\n" +
+"INSERT INTO `kitatms`.`enrollment` (`accountID`, `courseID`) VALUES ('1181100020', 'PSP5521');\n" +
+"INSERT INTO `kitatms`.`enrollment` (`accountID`, `courseID`) VALUES ('1181100022', 'PSP5521');\n" +
+"INSERT INTO `kitatms`.`enrollment` (`accountID`, `courseID`) VALUES ('1181100017', 'PSP5521');\n" +
+"INSERT INTO `kitatms`.`enrollment` (`accountID`, `courseID`) VALUES ('1181100016', 'PSP5521');\n" +
+"INSERT INTO `kitatms`.`enrollment` (`accountID`, `courseID`) VALUES ('1181100012', 'PSP5521');\n" +
+"INSERT INTO `kitatms`.`enrollment` (`accountID`, `courseID`) VALUES ('1181101286', 'PSP5521');\n" +
+"INSERT INTO `kitatms`.`enrollment` (`accountID`, `courseID`) VALUES ('1181100015', 'PSP5521');\n" +
+"INSERT INTO `kitatms`.`enrollment` (`accountID`, `courseID`) VALUES ('1181100021', 'PSP5521');\n" +
+"INSERT INTO `kitatms`.`enrollment` (`accountID`, `courseID`) VALUES ('1181101272', 'PSP5521');";
+                stat.execute(dbop);
+                
                 System.out.println("Dummy data successfully inserted into program database.");
             
         } catch (SQLException ex) {

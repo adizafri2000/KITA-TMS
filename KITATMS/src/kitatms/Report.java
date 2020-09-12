ad3078d72954d874f9de6c67c8414269615f06f2
adizafri2000 <61119610+adizafri2000@users.noreply.github.com>
2020-09-13 03:43:51 +0800

    Create Report.java

---
+++ KITATMS/src/kitatms/Report.java
@@ -0,0 +1,19 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kitatms;
+
+import com.itextpdf.text.Document;
+
+/**
+ *
+ * @author adiza
+ */
+public class Report {
+    public Report(){
+        Document d = new Document();
+    }
+}

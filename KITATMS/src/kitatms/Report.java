ab626ed72a55b6ddecd85c120c916d303b5409aa
adizafri2000 <61119610+adizafri2000@users.noreply.github.com>
2020-09-25 02:06:17 +0800

    Update Report.java

--- KITATMS/src/kitatms/Report.java
+++ KITATMS/src/kitatms/Report.java
@@ -7,13 +7,55 @@
 package kitatms;
 
 import com.itextpdf.text.Document;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  *
  * @author adiza
  */
 public class Report {
-    public Report(){
-        Document d = new Document();
+    private Course course;
+    private Trainer trainer;
+    private ArrayList<Trainee> traineeList;
+    private ArrayList<String> traineeID;
+    private DBConnection con;
+    
+    public Report(){}
+    
+    public void setConnection(DBConnection con){
+        this.con = con;
     }
+    
+    public void setCourse(Course c){
+        course = c;
+    }
+    
+    public void setTrainer(Trainer t){
+        trainer = t;
+    }
+    
+    public void setTraineeID(Course c) throws SQLException{
+        String courseID = c.getCourseID();
+        String query = "select * from enrollment where courseID='"+courseID+"';";
+        traineeID = con.retrieve(query, "accountID;");
+    }
+    
+    public void setTraineeList(ArrayList<Trainee> t){
+        traineeList = t;
+    }
+    
+    public Course getCourse(){
+        return course;
+    }
+    
+    public Trainer getTrainer(){
+        return trainer;
+    }
+    
+    public void generateReport(){
+        //itextpdf document generation here
+    }
+    
+    
 }
